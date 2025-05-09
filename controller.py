1b4b5dc851f47957020ae42db9e624061fa0af45
aMrnoob <0365946542az@gmail.com>
2025-05-10 00:08:29 +0700

    fix UI controller

--- controller.py
+++ controller.py
@@ -24,7 +24,7 @@ def add_custom_css():
         
         /* Card style cho các section */
         .card {
-            background-color: white;
+            background-color: #99CC99;
             border-radius: 10px;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
             padding: 20px;
